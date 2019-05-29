f8cdbdf7cc1c14b29985ba6254a0a6935594aed0
PagoDingo <daniel.danielarchibald@gmail.com>
2019-05-28 17:29:37 -0700

    Work on aa_questions project

---
+++ AA_Questions/questionsDB.rb
@@ -0,0 +1,224 @@
+require 'sqlite3'
+require 'singleton'
+require 'byebug'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+
+class User
+  attr_accessor :id, :fname, :lname
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM 
+        users
+      WHERE
+      id = ? 
+    SQL
+    User.new(data)
+  end
+
+  def self.find_by_name(fname, lname)
+    data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM 
+        users
+      WHERE
+      fname = ? AND lname = ?
+    SQL
+    User.new(data)
+  end
+
+  def authored_questions
+    Question.find_by_author_id(self.id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(self.id)
+  end
+end
+
+
+class Question
+  attr_accessor :id, :title, :body, :author_id
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM 
+        questions
+      WHERE
+      id = ? 
+    SQL
+    data.map { |row| Reply.new(row)}
+  end
+
+  def self.find_by_author_id(author_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM 
+        questions
+      WHERE
+        author_id = ? 
+    SQL
+    data.map { |row| Question.new(row) }
+  end
+
+  def author
+    User.find_by_id(self.author_id)
+  end
+  
+  def replies
+    Reply.find_by_user_id(self.author_id)
+  end
+
+end
+
+
+class QuestionFollow
+  attr_accessor :id, :user_id, :question_id
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM 
+        question_follows
+      WHERE
+      id = ? 
+    SQL
+    QuestionFollow.new(data)
+  end
+end
+
+
+class Reply
+  attr_accessor :id, :question_id, :parent_reply_id, :author_id, :body
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @parent_reply_id = options['parent_reply_id']
+    @author_id = options['author_id']
+    @body = options['body']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM 
+        replies
+      WHERE
+      id = ? 
+    SQL
+    data.map { |row| Reply.new(row) }
+  end
+
+  def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM 
+        replies
+      WHERE
+        user_id = ? 
+    SQL
+    data.map { |row| Reply.new(row)}
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM 
+        replies
+      WHERE
+        question_id = ? 
+    SQL
+    data.map { |row| Reply.new(row)}
+  end
+
+  def author
+    Question.find_by_author_id(self.author_id)
+  end
+
+  def question
+    Question.find_by_question_id(self.question_id)
+  end
+
+  def parent_reply
+    Reply.find_by_id(parent_reply_id)
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class QuestionLike
+  attr_accessor :id, :user_id, :question_id
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM 
+        question_likes
+      WHERE
+      id = ? 
+    SQL
+    QuestionLike.new(data)
+  end
+end
