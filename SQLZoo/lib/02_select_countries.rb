b7e94b5216c8f1e7ba1aa3249b6e8411972f5f5f
App Academy Student <student@appacademy.io>
2019-05-28 18:06:10 -0700

    Add SQLZoo

--- SQLZoo/lib/02_select_countries.rb
+++ SQLZoo/lib/02_select_countries.rb
@@ -25,12 +25,12 @@ def large_countries
   # Show the names of the countries that have a population of at least
   # 200 million. 200 million is 200,000,000 (eight zeros).
   execute(<<-SQL)
-    SELECT 
+    SELECT
       name
-    FROM 
-      countries 
+    FROM
+      countries
     WHERE 
-      population > 200000000
+      population >= 200000000;
   SQL
 end
 
@@ -38,12 +38,12 @@ def high_population_gdps
   # Give the names and the per capita GDPs of countries with a population
   # of at least 200 million.
   execute(<<-SQL)
-  SELECT 
-    name, gdp/population
-  FROM 
-    countries
-  WHERE
-    population >= 200000000
+    SELECT
+      name, gdp / population
+    FROM
+      countries
+    WHERE
+      population >= 200000000;
   SQL
 end
 
@@ -53,38 +53,34 @@ def population_in_millions
   # millions.
   execute(<<-SQL)
     SELECT
-      name, population/1000000
-    FROM 
+      name, population / 1000000
+    FROM
       countries
-    WHERE 
-      continent = 'South America'
+    WHERE
+      continent = 'South America';
   SQL
 end
 
 def name_and_population
   # Show the name and population for 'France', 'Germany', and 'Italy'
   execute(<<-SQL)
-  SELECT
-    name, population
-  FROM 
-    countries
-  WHERE
-    name = 'France'
-    OR 
-    name = 'Germany'
-    OR 
-    name = 'Italy'
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE
+      name = 'France' OR name = 'Germany' OR name = 'Italy';
   SQL
 end
 
 def united_we_stand
   # Show the countries that have a name that includes the word 'United'
   execute(<<-SQL)
-  SELECT
-    name 
-  FROM 
-    countries
-  WHERE
-    name LIKE 'United%'
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      name LIKE 'United%';
   SQL
 end
--- SQLZoo/lib/03_select_nobel.rb
+++ SQLZoo/lib/03_select_nobel.rb
@@ -24,38 +24,36 @@ end
 def prizes_from_1950
   # Display Nobel prizes for 1950.
   execute(<<-SQL)
-    SELECT 
-      *
+    SELECT
+      yr, subject, winner
     FROM
       nobels
-    WHERE 
-      yr = 1950
+    WHERE
+      yr = 1950;
   SQL
 end
 
 def literature_1962
   # Show who won the 1962 prize for Literature.
   execute(<<-SQL)
-    SELECT 
+    SELECT
       winner
-    FROM 
+    FROM
       nobels
-    WHERE 
-      subject = 'Literature'
-    AND 
-      yr = 1962
+    WHERE
+      yr = 1962 AND subject = 'Literature';
   SQL
 end
 
 def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
-    SELECT 
+    SELECT
       yr, subject
-    FROM 
+    FROM
       nobels
     WHERE
-      winner = 'Albert Einstein'
+      winner = 'Albert Einstein';
   SQL
 end
 
@@ -64,12 +62,10 @@ def millennial_peace_prizes
   execute(<<-SQL)
     SELECT
       winner
-    FROM 
+    FROM
       nobels
-    WHERE 
-      yr >= 2000
-    AND
-      subject = 'Peace'
+    WHERE
+      subject = 'Peace' AND yr >= 2000;
   SQL
 end
 
@@ -77,42 +73,36 @@ def eighties_literature
   # Show all details (yr, subject, winner) of the Literature prize winners
   # for 1980 to 1989 inclusive.
   execute(<<-SQL)
-  SELECT
-    *
-  FROM 
-    nobels
-  WHERE 
-    subject = 'Literature'
-  AND 
-    yr BETWEEN 1980 AND 1989
-    SQL
+    SELECT
+      yr, subject, winner
+    FROM
+      nobels
+    WHERE
+      subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
+  SQL
 end
 
 def presidential_prizes
   # Show all details of the presidential winners: ('Theodore Roosevelt',
   # 'Woodrow Wilson', 'Jimmy Carter')
   execute(<<-SQL)
-    SELECT 
-      *
-    FROM 
+    SELECT
+      yr, subject, winner
+    FROM
       nobels
-    WHERE 
-      winner = 'Theodore Roosevelt'
-    OR 
-      winner = 'Woodrow Wilson'
-    OR 
-      winner = 'Jimmy Carter'
+    WHERE
+      winner = 'Theodore Roosevelt' OR winner= 'Woodrow Wilson' OR winner='Jimmy Carter';
   SQL
 end
 
 def nobel_johns
   # Show the winners with first name John
   execute(<<-SQL)
-    SELECT 
+    SELECT
       winner
-    FROM 
+    FROM
       nobels
     WHERE
-      winner LIKE 'John%'
+      winner LIKE 'John%';
   SQL
 end
